7392013ba87732459d80cd6f183411d3d51cae1f
lilia <ribac.lilia@gmail.com>
2024-04-15 15:19:16 -0500

    add the isShowing function

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,6 +1,13 @@
 const knex = require('../db/connection');
 
-function list() {
+function list(isShowing) {
+  if (isShowing) {
+    return knex('movies')
+      .join(movies_theaters, movies.movie_id, movies_theaters.movie_id)
+      .select('movies. *')
+      .where({ is_showing: true })
+      .groupBy('movies.movie_id');
+  }
   return knex('movies').select('*');
 }
 
