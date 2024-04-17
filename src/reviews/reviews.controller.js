ef7e87bb5d9cb2a95158736d1345c2e1b21d79dd
lilia <ribac.lilia@gmail.com>
2024-04-17 17:51:43 -0500

    add the update function

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,5 +1,5 @@
 const reviewsService = require('./reviews.service.js');
-const asyncErrorBoundary = require('../errors/asyncErrorBoundary.js');
+const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
 
 async function reviewsExists(req, res, next) {
   const { reviewId } = req.params;
@@ -13,6 +13,16 @@ async function reviewsExists(req, res, next) {
   return next({ status: 404, message: `Review cannot be found.` });
 }
 
+async function update(req, res) {
+  const updatedReview = {
+    ...res.locals.review,
+    ...req.body.data,
+    review_id: res.locals.review.review_id,
+  };
+  const data = await reviewsService.update(updatedReview);
+  res.json({ data });
+}
+
 async function destroy(req, res) {
   const reviewId = req.params.reviewId;
   await reviewsService.delete(reviewId);
@@ -21,4 +31,5 @@ async function destroy(req, res) {
 
 module.exports = {
   delete: [asyncErrorBoundary(reviewsExists), asyncErrorBoundary(destroy)],
+  update: [asyncErrorBoundary(reviewsExists), asyncErrorBoundary(update)],
 };
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -4,10 +4,19 @@ function destroy(reviewId) {
   return knex('reviews').where({ review_id: reviewId }).del();
 }
 
+function update(updateReviews) {
+  return knex('reviews')
+    .where({ review_id: updateReviews.review_id })
+    .update(updateReviews, '*')
+    .then((updatedRecords) => updatedRecords[0]);
+}
+
 function read(reviewId) {
   return knex('reviews').select('*').where({ review_id: reviewId }).first();
 }
+
 module.exports = {
   read,
+  update,
   delete: destroy,
 };
