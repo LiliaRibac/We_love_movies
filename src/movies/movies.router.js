7b5c659d379162f262f200697ec199908044e9bd
lilia <ribac.lilia@gmail.com>
2024-04-18 15:06:42 -0500

    add methodNotAllowed

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const controller = require('./movies.controller');
-// const methodNotAllowed = require('../errors/methodNotAllowed');
+const methodNotAllowed = require('../errors/methodNotAllowed');
 
 router.route('/:movieId/theaters').get(controller.readTheatersMovies);
 router.route('/:movieId/reviews').get(controller.readReviewsMovies);
 
-router.route('/:movieId').get(controller.read);
-router.route('/').get(controller.list);
+router.route('/:movieId').get(controller.read).all(methodNotAllowed);
+router.route('/').get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
